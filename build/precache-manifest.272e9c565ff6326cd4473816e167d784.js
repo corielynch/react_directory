46092aa03a4f7f59178a1bdd5b65a9bcb71b3820
Corie Lynch <Lynchc17@gmail.com>
2020-08-17 18:29:38 -0700

    Restructuring files

---
+++ build/precache-manifest.272e9c565ff6326cd4473816e167d784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542104ee476fac04e1636ec19d2c47a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9128a5d823358a0e9614",
+    "url": "/static/css/main.bbb0c916.chunk.css"
+  },
+  {
+    "revision": "d086968ba2a3e8b1b9f0",
+    "url": "/static/js/2.781b3cf1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.781b3cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9128a5d823358a0e9614",
+    "url": "/static/js/main.66950906.chunk.js"
+  },
+  {
+    "revision": "1c5ef2906b6b7e1b288c",
+    "url": "/static/js/runtime-main.6127c2cf.js"
+  }
+]);
